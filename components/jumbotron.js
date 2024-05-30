7df19b842c5bc0ce9f4a9e1e1ff6f66b0b7653e9
arduzj <arduzj@gmail.com>
2024-05-30 17:40:43 -0400

    Mensaje de bienvenida traducido

--- components/jumbotron.js
+++ components/jumbotron.js
@@ -21,7 +21,7 @@ const Jumbotron = () =>
         <div className="jumbotron jumbotron-fluid bg-dark mb-0" alt="Photo by Benjamin Child on Unsplash - https://unsplash.com/photos/GWe0dlVD9e0">
             <div className="container text-white">
                 <h1 className="display-2 text-white d-none d-lg-block">
-                    Elevate your work style
+                    Eleva tu estilo de trabajo
                 </h1>
                 <h1 className="display-4 text-white text-center d-lg-none">
                     Elevate your work style
